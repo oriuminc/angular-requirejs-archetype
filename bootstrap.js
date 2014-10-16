ee6e79e8aefe3e585e899f56ccbc9407b3794dac
Ates Goral <ates.g@myplanetdigital.com>
2014-10-16 12:53:24 -0400

    Added UI routing

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,23 @@
 define([
-	'angular'
+	'angular',
+	'angular-ui-router'
 ], function (angular) {
-	angular.module('app', []);
+	'use strict';
+
+	angular.module('app', [ 'ui.router' ])
+        .config([ '$urlRouterProvider', function ($urlRouterProvider) {
+            $urlRouterProvider.otherwise('/');
+        }])
+		.controller('helloController', [ '$scope', function ($scope) {
+			$scope.yourNameHere = 'Flat Eric';
+		}])
+		.config([ '$stateProvider', function ($stateProvider) {
+			$stateProvider.state('main', {
+				url: '/',
+				template: '<h1>Hello Again, {{ yourNameHere }}.</h1>',
+				controller: 'helloController'
+			});
+		}]);
 
 	angular.bootstrap(document, [ 'app' ]);
 });
--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 requirejs.config({
 	paths: {
-		'angular': 'bower_components/angular/angular'
+		'angular': 'bower_components/angular/angular',
+		'angular-ui-router': 'bower_components/angular-ui-router/release/angular-ui-router'
 	},
 	shim: {
 		'angular': {
 			exports: 'angular'
+		},
+		'angular-ui-router': {
+			deps: [ 'angular' ]
 		}
 	},
 	deps: [ 'bootstrap' ]
