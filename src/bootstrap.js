b3489e1205ed9e0a148e9d89a1a8a263f394588d
Ates Goral <ates.g@myplanetdigital.com>
2014-11-06 14:01:49 -0500

    Moved source files into folders

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,8 @@
 define([
     'angular',
     'app',
-    'login',
-    'portal'
+    'login/login',
+    'portal/portal'
 ], function (angular, app) {
     'use strict';
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 requirejs.config({
     paths: {
-        'text': 'bower_components/requirejs-text/text',
-        'angular': 'bower_components/angular/angular',
-        'angular-ui-router': 'bower_components/angular-ui-router/release/angular-ui-router'
+        'text': '../bower_components/requirejs-text/text',
+        'angular': '../bower_components/angular/angular',
+        'angular-ui-router': '../bower_components/angular-ui-router/release/angular-ui-router'
     },
     shim: {
         'angular': {
